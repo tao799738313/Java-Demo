1bce0f897bb4eda5605be5de134a94b618c7f254
pdt <799738313@qq.com>
2020-07-23 14:35:24 +0800

    9002使用了lombok

--- springcloud-pdt/module-order-9002/src/main/java/com/pdt/moduleorder9002/controller/test.java
+++ springcloud-pdt/module-order-9002/src/main/java/com/pdt/moduleorder9002/controller/test.java
@@ -2,6 +2,7 @@ package com.pdt.moduleorder9002.controller;
 
 import com.pdt.common.redis.service.RedisService;
 import com.pdt.moduleorder9002.vo.HttpResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Slf4j
 @Controller
 @ResponseBody
 public class test {
@@ -19,8 +20,17 @@ public class test {
 
     @GetMapping("/")
     public String main(String[] args) {
+
+        log.trace("这是trance日志。。。。");
+        log.debug("这是debug日志。。。。");
+        log.info("这是info日志。。。。");
+        log.warn("这是warn日志。。。。");
+        log.error("这是error日志。。。。");
+
         return "order-9002";
     }
+
+
 
     @GetMapping("/redis")
     public String redis(String[] args) {
