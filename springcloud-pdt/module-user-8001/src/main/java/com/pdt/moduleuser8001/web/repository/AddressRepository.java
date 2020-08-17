800dbe5ed254d9764a6960fecb86159106e03ab7
pdt <799738313@qq.com>
2020-08-17 18:52:35 +0800

    push

--- springcloud-pdt/module-user-8001/src/main/java/com/pdt/moduleuser8001/web/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pdt.moduleuser8001.web.repository;
-
-import com.pdt.moduleuser8001.web.bean.Address;
-import com.pdt.moduleuser8001.web.bean.Item;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface AddressRepository extends ElasticsearchRepository<Address,Long> {
-    // 留空
-}
--- springcloud-pdt/module-user-8001/src/main/java/com/pdt/moduleuser8001/web/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pdt.moduleuser8001.web.repository;
-
-import com.pdt.moduleuser8001.web.bean.Item;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
-    // 留空
-}
